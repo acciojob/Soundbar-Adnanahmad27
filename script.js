db684c1952997c459aadf5f60024329505c7d1bb
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-03-01 00:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+/**/
+
+let currentAudio;
+
+function playSound(soundId) {
+  stopAudio(); // Stop any currently playing audio
+  
+  const audio = document.getElementById(soundId);
+  audio.play();
+  currentAudio = audio;
+}
+
+function stopSound() {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+}
+
